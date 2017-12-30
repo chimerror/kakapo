b54e3b29325dbea5ea25b01b42bc45ac69977c38
Jason Mitchell <foobardog@hotmail.com>
2017-12-30 13:31:28 -0800

    Added Camera, Hooked up to TV

---
+++ Assets/TvController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TvController : MonoBehaviour
+{
+    public Material offMaterial;
+    public Material onMaterial;
+
+
+    private MeshRenderer _meshRenderer;
+    private bool _powerOn = false;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void AlternatePower()
+    {
+        var currentMaterials = _meshRenderer.materials;
+        if (_powerOn)
+        {
+            currentMaterials[1] = offMaterial;
+        }
+        else
+        {
+            currentMaterials[1] = onMaterial;
+        }
+
+        _meshRenderer.materials = currentMaterials;
+        _powerOn = !_powerOn;
+    }
+}
